57b4f9bf2f6561a9bfa3e077e6862c36288b6ece
Thiague <tfcbusy@yahoo.com>
2019-04-21 23:15:00 -0300

    Updates

---
+++ precache-manifest.504b2b2c4fd246f60bcef1f4e10a4836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c5e07a032a14eebd4e",
+    "url": "/contactmanager/static/js/main.0fe00291.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "3e12a60a646dd19570ce",
+    "url": "/contactmanager/static/css/2.909a1023.chunk.css"
+  },
+  {
+    "revision": "3e12a60a646dd19570ce",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "8ab2ff134f6e8856a2f72dfc4bbb60fb",
+    "url": "/contactmanager/index.html"
+  }
+];
